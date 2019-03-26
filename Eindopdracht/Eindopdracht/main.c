f47f180e42d9e59ec693c544ad492862826a3fb8
Maurits <mauricebrouwers373@hotmail.com>
2019-03-26 15:05:48 +0100

    empty project

---
+++ Eindopdracht/Eindopdracht/main.c
@@ -0,0 +1,18 @@
+/*
+ * Eindopdracht.c
+ *
+ * Created: 26-3-2019 15:05:34
+ * Author : mauri
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
